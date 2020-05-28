9047992d5496215ae0a9cbc6c03cfb257a04b943
github username <you@example.com>
2020-05-27 17:28:45 -0700

    created Homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_27_003705) do
 
+  create_table "athlete_sports", force: :cascade do |t|
+    t.integer "athlete_id", null: false
+    t.integer "sport_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["athlete_id"], name: "index_athlete_sports_on_athlete_id"
+    t.index ["sport_id"], name: "index_athlete_sports_on_sport_id"
+  end
+
+  create_table "athletes", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.string "email"
+    t.integer "coach_id", null: false
+    t.integer "program_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["coach_id"], name: "index_athletes_on_coach_id"
+    t.index ["program_id"], name: "index_athletes_on_program_id"
+  end
+
+  create_table "coach_sports", force: :cascade do |t|
+    t.integer "coach_id", null: false
+    t.integer "sport_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["coach_id"], name: "index_coach_sports_on_coach_id"
+    t.index ["sport_id"], name: "index_coach_sports_on_sport_id"
+  end
+
+  create_table "coaches", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "programs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "price"
+    t.integer "coach_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["coach_id"], name: "index_programs_on_coach_id"
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "athlete_sports", "athletes"
+  add_foreign_key "athlete_sports", "sports"
+  add_foreign_key "athletes", "coaches"
+  add_foreign_key "athletes", "programs"
+  add_foreign_key "coach_sports", "coaches"
+  add_foreign_key "coach_sports", "sports"
+  add_foreign_key "programs", "coaches"
 end
